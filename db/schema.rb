6d4523022dd9a9b49150d738b1d08905a47944c8
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-24 17:08:21 +0100

    - add sessio controller and code' - init pusher and update online status realtime - add user model is_logged_in column and add validation - update routes to use session controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524115940) do
+ActiveRecord::Schema.define(version: 20180524154037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180524115940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "is_signed_in", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
