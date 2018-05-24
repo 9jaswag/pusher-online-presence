e215bfb8da6fa3a8619d089c00d8776a3a1115b2
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-24 17:08:21 +0100

    - add sessio controller and code' - init pusher and update online status realtime - add user model is_logged_in column and add validation - update routes to use session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates :is_signed_in, inclusion: [true, false]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
