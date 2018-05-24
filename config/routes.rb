7aa3ef42b2bf04b7c76b77a67da9858f70c60a47
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-24 17:08:21 +0100

    - add sessio controller and code' - init pusher and update online status realtime - add user model is_logged_in column and add validation - update routes to use session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :sessions => "sessions" }
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
